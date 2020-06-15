37662be8f45df52c1574cb092666e344002453f6
Andrei Mironov <amironov88@yandex.ru>
2020-06-15 13:37:35 +0400

    2-я проверка

--- scripts/script.js
+++ scripts/script.js
@@ -3,31 +3,29 @@ let popup = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 let inputInfo = document.querySelectorAll('.popup__input');
-let closeButton = document.querySelector('.popup__close-icon');
+let closeButton = document.querySelector('.popup__close-btn');
+let nameInput = document.querySelector('.popup__input_info_name');
+let jobInput = document.querySelector('.popup__input_info_about');
+let formElement = document.querySelector('.popup__form');
 
-
-inputInfo[0].value = profileName.textContent;
-inputInfo[1].value = profileProfession.textContent;
 
 function openPopup () {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-let formElement = document.querySelector('.popup__form');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let nameInput = document.querySelector('.popup__input_info_name');
-  let jobInput = document.querySelector('.popup__input_info_about');
   profileName.textContent = nameInput.value;
   profileProfession.textContent = jobInput.value;
   closePopup();
 }
-
 
 
 editButton.addEventListener('click', openPopup);
